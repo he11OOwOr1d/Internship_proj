24c27f6b794e96761134532f643a49dee279d251
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-09-18 21:13:52 +0530

    added cors

--- Intership Project/src/utils/constants.js
+++ Intership Project/src/utils/constants.js
@@ -5,4 +5,4 @@ export const RES_IMAGE =
 `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`
 
 export const MENU_API =
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.9905081&lng=76.9873477&restaurantId="
+"https://corsproxy.io/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.9905081&lng=76.9873477&restaurantId="
